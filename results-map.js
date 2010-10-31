9245e8e5f06a25b1572b976784798c154be17995
judebritto <judebritto@google.com>
2010-10-31 17:04:19 +0000

    Turn off static map

--- results-map.js
+++ results-map.js
@@ -56,8 +56,9 @@ var $window = $(window), ww = $window.width(), wh = $window.height();
 var prefs = new _IG_Prefs();
 
 var opt = window.GoogleElectionMapOptions || {};
-opt.static1 = ( ww == 573  &&  wh == 463 );
-opt.static = opt.static1  ||  ( ww == 620  &&  wh == 480 );
+// opt.static1 = ( ww == 573  &&  wh == 463 );
+// opt.static = opt.static1  ||  ( ww == 620  &&  wh == 480 );
+opt.static = false;
 opt.fontsize = '15px';
 var sw = opt.panelWidth = 180;
 
